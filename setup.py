127cd412183db7eb2c0c775ca50c7a33a1dd71c4
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2020-06-24 10:05:24 -0700

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Nv7-GitHub/GPGame",
-    packages=setuptools.find_packages(),
+    packages=["Kivy"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
