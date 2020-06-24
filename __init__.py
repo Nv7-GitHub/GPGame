c800a185ae81f303647121e6ca71de4a9f1962a8
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2020-06-24 10:02:42 -0700

    Added files for v0.0.1

---
+++ __init__.py
@@ -0,0 +1,4 @@
+K_RIGHT = chr(275)
+K_LEFT = chr(276)
+K_DOWN = chr(274)
+K_UP = chr(273)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GPGame-Nv7",
+    version="0.0.1",
+    author="Nishant Vikramaditya",
+    author_email="junk4Nv7@gmail.com",
+    description="An abstraction layer on the Kivy GPU accelerated engine.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
