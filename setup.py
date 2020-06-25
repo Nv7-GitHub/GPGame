c788c3de469f1f4eb43f8406bda8a2b237c4223f
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2020-06-24 20:14:30 -0700

    Add working setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from distutils.core import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="GPGame-Nv7",
+setup(
+    name="GPGame",
     version="0.0.1",
     author="Nishant Vikramaditya",
     author_email="junk4Nv7@gmail.com",
@@ -12,11 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Nv7-GitHub/GPGame",
-    packages=["Kivy"],
+    packages=["GPGame"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=["Kivy"]
 )
