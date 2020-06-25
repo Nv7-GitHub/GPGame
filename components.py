23aa908c6f04e22a23462f8d1fb6a8d0a474d344
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2020-06-24 20:15:18 -0700

    Added package

---
+++ components.py
@@ -0,0 +1,95 @@
+from kivy.graphics import Rectangle, Ellipse
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.graphics import Color
+from kivy.uix.widget import Widget
+
+
+class Rect(Widget):
+    def __init__(self, x, y, width, height, color, **kwargs):
+        super(Rect, self).__init__()
+        self.size = (width, height)
+        self.pos = (x, y)
+        self.kcol = Color(color[0], color[1], color[2])
+        self.kobj = Rectangle(pos=self.pos, size=self.size, **kwargs)
+        self.ktype = "GP"
+
+    def resize(self, width, height):
+        self.size = (width, height)
+        self.kobj.size = self.size
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.kobj.pos = self.pos
+
+
+class Oval(Widget):
+    def __init__(self, x, y, width, height, color, **kwargs):
+        super(Oval, self).__init__()
+        self.size = (width, height)
+        self.pos = (x, y)
+        self.kcol = Color(color[0], color[1], color[2])
+        self.kobj = Ellipse(pos=self.pos, size=self.size, **kwargs)
+        self.ktype = "GP"
+
+    def resize(self, width, height):
+        self.size = (width, height)
+        self.kobj.size = self.size
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.kobj.pos = self.pos
+
+
+class Text(Widget):
+    def __init__(self, text, **kwargs):
+        super(Text, self).__init__()
+        self.text = text
+        self.kobj = Label(text=self.text, pos=self.pos, **kwargs)
+        self.ktype = "WGP"
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.kobj.pos = self.pos
+
+    def set_text(self, text):
+        self.text = text
+        self.kobj.text = self.text
+
+
+class Img(Widget):
+    def __init__(self, imagepath, **kwargs):
+        super(Img, self).__init__()
+        self.kobj = Image(source=imagepath, pos=self.pos, size=self.size, **kwargs)
+        self.ktype = "WGP"
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.kobj.pos = self.pos
+
+    def resize(self, width, height):
+        self.size = (width, height)
+        self.kobj.size = self.size
+
+
+class Btn(Widget):
+    def __init__(self, text, onclick, x, y, width, height, **kwargs):
+        super(Btn, self).__init__()
+        self.text = text
+        self.pos = (x, y)
+        self.size = (width, height)
+        self.kobj = Button(pos=self.pos, size=self.size, text=self.text, on_press=onclick, **kwargs)
+        self.ktype = "WGP"
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.kobj.pos = self.pos
+
+    def resize(self, width, height):
+        self.size = (width, height)
+        self.kobj.size = self.size
+
+    def set_text(self, text):
+        self.text = text
+        self.kobj.text = text
---
+++ engine.py
@@ -0,0 +1,57 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.core.window import Window
+
+
+class GPGame(Widget):
+    def __init__(self, **kwargs):
+        super(GPGame, self).__init__(**kwargs)
+        self.tick = None
+        self.window = Window
+        self.mousepos = (0, 0)
+        self.keys_pressed = []
+        Window.bind(mouse_pos=self.kmousehandler, on_key_down=self.kkeyhandlerdown, on_key_up=self.kkeyhandlerup)
+
+    def set_tick(self, tickfunc, interval):
+        self.tick = tickfunc
+        Clock.schedule_interval(self.tick, interval)
+
+    def run(self, title):
+        runner = GPRunner(self, title)
+        runner.run()
+
+    def add_component(self, component):
+        if component.ktype == "GP":
+            self.canvas.add(component.kcol)
+            self.canvas.add(component.kobj)
+        elif component.ktype == "WGP":
+            self.add_widget(component.kobj)
+
+    def remove_component(self, component):
+        if component.ktype == "GP":
+            self.canvas.remove(component.kcol)
+            self.canvas.remove(component.kobj)
+        elif component.ktype == "WGP":
+            self.remove_widget(component.kobj)
+
+    def kmousehandler(self, w, p):
+        self.mousepos = p
+
+    def kkeyhandlerdown(self, keyboard, keycode, whoknows, key, modifiers):
+        self.keys_pressed.append(chr(keycode))
+        print(keycode)
+
+    def kkeyhandlerup(self, a, b, c):
+        del self.keys_pressed[self.keys_pressed.index(chr(b))]
+
+
+class GPRunner(App):
+    def __init__(self, game, title):
+        super(GPRunner, self).__init__()
+
+        self.title = title
+        self.game = game
+
+    def build(self):
+        return self.game
